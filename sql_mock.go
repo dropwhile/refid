55d92995963e99222249475f52405f3e1b02dca8
elij <elij@wundrd.net>
2023-09-24 20:58:16 -0700

    add docstring

--- sql_mock.go
+++ sql_mock.go
@@ -1,9 +1,21 @@
 package refid
 
+// A matcher that supports the pgxmock.Argument interface
 type AnyMatcher struct {
 	tag byte
 }
 
+// Create a AnyMatcher matcher that supports the pgxmock.Argument interface,
+// and matches against a specific Tag. Any valid RefIds that do not
+// match the tag specified, will be considered not matching.
+//
+// If tag is 0, will support matching any RefId (tag is then ignored)
+//
+// Example usage:
+//
+//	mock.ExpectQuery("^INSERT INTO some_table (.+)").
+//	 WithArgs(refid.MatchAny(1), 1).
+//	 WillReturnRows(rows)
 func MatchAny(tag byte) AnyMatcher {
 	return AnyMatcher{tag}
 }
