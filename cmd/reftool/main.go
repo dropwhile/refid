af1c0dd787e6338b68e2998bcdf661589b0207df
elij <elij@wundrd.net>
2023-12-06 20:11:25 -0800

    bump reftool version

--- cmd/reftool/main.go
+++ cmd/reftool/main.go
@@ -42,7 +42,7 @@ func main() {
 		kong.Description("A tool for working with refids"),
 		kong.UsageOnError(),
 		kong.Vars{
-			"version": "1.0.6",
+			"version": "1.0.7",
 		},
 	)
 	err := ctx.Run(&cli)
